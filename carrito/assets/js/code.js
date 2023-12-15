f7439552d59c7f3c7c9fd9f95269d5ddc3001202
Blo2SonNT <migustanoeto.2@gmail.com>
2023-12-14 21:41:42 -0500

    [+] Ayuda con los botones de incremento o decremento

--- carrito/assets/js/code.js
+++ carrito/assets/js/code.js
@@ -1,2 +1,19 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+let botonesControlCantidad = document.querySelectorAll(".botonControl")
+botonesControlCantidad.forEach(botoncito => {
+    botoncito.addEventListener("click", (evento) => {
+        let h5Producto = document.querySelector(`#${evento.target.dataset.idCantidad}`)
+        let cantidadActual = h5Producto.innerText
+        if (evento.target.dataset.accion == 'restar') {
+            if (cantidadActual > 1) {
+                cantidadActual = parseInt(cantidadActual) - 1
+            }
+        } else {
+            cantidadActual = parseInt(cantidadActual) + 1
+        }
+        h5Producto.innerHTML = cantidadActual
+    })
+});
