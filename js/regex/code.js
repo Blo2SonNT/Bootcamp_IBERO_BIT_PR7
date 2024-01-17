0abeec400cafb9f98738a13751d69ee5a50b942e
Blo2SonNT <migustanieto.2@gmail.com>
2024-01-17 18:09:56 -0500

    [+] Formularios reactivos - ANGULAR

---
+++ js/regex/code.js
@@ -0,0 +1,4 @@
+let regexAlfabetico = /^[A-Za-z ]+$/
+let texto = "Mi mama me mima"
+
+console.log(regexAlfabetico.test(texto));
--- pataSalud/src/app/components/contacto/contacto.component.ts
+++ pataSalud/src/app/components/contacto/contacto.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-contacto',
-  standalone: true,
-  imports: [],
-  templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.css'
+    selector: 'app-contacto',
+    standalone: true,
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
+    ],
+    templateUrl: './contacto.component.html',
+    styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
+
+    contactoForm: FormGroup
+    regexAlfabetico = /^[A-Za-z ]+$/
+    regexNumericos = /^[0-9]+$/
+
+
+    constructor(private fb: FormBuilder){
+        this.contactoForm = this.fb.group({
+            nombre: ['', [ Validators.required, Validators.pattern(this.regexAlfabetico) ]],
+            correo: ['', [Validators.required, Validators.minLength(6)]],
+            telefono: ['', [Validators.required, Validators.pattern(this.regexNumericos)]],
+            nombre_mascota: ['', [ Validators.required, Validators.pattern(this.regexAlfabetico) ]]
+        })
+    }
+
+    registrarContacto(){
+        console.log(this.contactoForm);
+    }
 
 }
